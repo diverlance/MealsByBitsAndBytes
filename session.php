aa7f26b7b083e9031abecf58a2ea291a7e20a081
Chiu350 <cheungfaat.chiu@sjsu.edu>
2015-12-10 12:59:48 -0800

    updated login session using PDO not mysql extension

--- session.php
+++ session.php
@@ -1,32 +1,36 @@
 <?php
-  $connection = mysql_connect("localhost", "cs174", "qweasdzxc");
-
-  $db = mysql_select_db("bitsnbytes", $connection);
+try
+{
+  $con = new PDO("mysql:host=localhost;dbname=bitsnbytes", "cs174", "qweasdzxc");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   session_start();
 
   $user_check=$_SESSION['login_user'];
-  $ses_sql=mysql_query("select email from users where email='$user_check'", $connection);
-  $row = mysql_fetch_assoc($ses_sql);
+
+  $ses_sql = $con->prepare("SELECT email FROM users WHERE email=:uemail");
+  $ses_sql->bindparam(":uemail", $user_check);
+  $ses_sql->execute();
+
+  $row = $ses_sql->fetch(PDO::FETCH_ASSOC);
   $login_session =$row['email'];
   if(!isset($login_session))
   {
     mysql_close($connection);
     header('Location: index.php');
   }
-  $ses_sql=mysql_query("select * from users where email='$user_check'", $connection);
-  $row = mysql_fetch_assoc($ses_sql);
-  $userID_session =$row['id'];
-  $first_session =$row['first_name'];
-  $last_session =$row['last_name'];
-  $email_session =$row['email'];
+  $ses_sql = $con->prepare("SELECT * FROM users WHERE email=:uemail");
+  $ses_sql->bindparam(":uemail", $user_check);
+  $ses_sql->execute();
 
-  try
-  {
-    $con = new PDO("mysql:host=localhost;dbname=bitsnbytes", "cs174", "qweasdzxc");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(PDOException $e)
-  {
+  $row = $ses_sql->fetch(PDO::FETCH_ASSOC);
 
-  }
+  $userID_session = $row['id'];
+  $first_session = $row['first_name'];
+  $last_session = $row['last_name'];
+  $email_session = $row['email'];
+}
+catch(PDOException $e)
+{
+  
+}
 ?>
